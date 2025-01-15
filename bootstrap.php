b80e8329b2199047c7af49cecb1477213606481b
d.savuljesku <d.savuljesku@gmail.com>
2025-01-15 14:05:46 +0100

    Fix direct access to

--- bootstrap.php
+++ bootstrap.php
@@ -12,17 +12,20 @@ Bootstrapper::getInstance()
 	->register( 'wikicron', static function () {
 		$GLOBALS['wgServiceWiringFiles'][] = __DIR__ . '/includes/ServiceWiring.php';
 
-		$GLOBALS['wgHooks']['LoadExtensionSchemaUpdates'][] = static function ( DatabaseUpdater $updater ) {
-			$dbType = $updater->getDB()->getType();
+		$GLOBALS['wgExtensionFunctions'][] = static function() {
+			$hookContainer = \MediaWiki\MediaWikiServices::getInstance()->getHookContainer();
+			$hookContainer->register( 'LoadExtensionSchemaUpdates', static function ( DatabaseUpdater $updater ) {
+				$dbType = $updater->getDB()->getType();
 
-			$updater->addExtensionTable(
-				'wiki_cron',
-				__DIR__ . '/db/' . $dbType . '/wiki-cron.sql'
-			);
-			$updater->addExtensionTable(
-				'wiki_cron_history',
-				__DIR__ . '/db/' . $dbType . '/wiki-cron.sql'
-			);
+				$updater->addExtensionTable(
+					'wiki_cron',
+					__DIR__ . '/db/' . $dbType . '/wiki-cron.sql'
+				);
+				$updater->addExtensionTable(
+					'wiki_cron_history',
+					__DIR__ . '/db/' . $dbType . '/wiki-cron.sql'
+				);
+			} );
 		};
 		$GLOBALS['mwsgProcessManagerPlugins']['wikicron'] = [
 			'class' => 'MWStake\MediaWiki\Component\WikiCron\WikiCronPlugin',
