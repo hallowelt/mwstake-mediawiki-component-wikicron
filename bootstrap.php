d46bff9acc6af461c4ca2400bf42d46d1593b9d6
Robert Vogel <1201528+osnard@users.noreply.github.com>
2025-03-24 15:31:01 +0100

    Bump version

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKICRON_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKICRON_VERSION', '1.0.5' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKICRON_VERSION', '1.0.6' );
 
 Bootstrapper::getInstance()
 	->register( 'wikicron', static function () {
